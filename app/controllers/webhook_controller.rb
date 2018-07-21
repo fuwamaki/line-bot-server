9fad852d0a755a9025e358b48959ff594e90dca5
fuwamaki <yusaku_maki@fablic.co.jp>
2018-07-22 02:19:50 +0900

    webhook_controller.rb書き換え

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,39 +1,45 @@
 class WebhookController < ApplicationController
-    protect_from_forgery with: :null_session # CSRF対策無効化
+    // Lineからのcallbackか認証
+    protect_from_forgery with: :null_session
   
-    CHANNEL_ID = ENV['LINE_CHANNEL_ID']
-    CHANNEL_SECRET = ENV['LINE_CHANNEL_SECRET']
-    OUTBOUND_PROXY = ENV['LINE_OUTBOUND_PROXY']
+    CHANNEL_SECRET = ENV['CHANNEL_SECRET']
+    OUTBOUND_PROXY = ENV['OUTBOUND_PROXY']
+    CHANNEL_ACCESS_TOKEN = ENV['CHANNEL_ACCESS_TOKEN']
   
     def callback
       unless is_validate_signature
         render :nothing => true, status: 470
       end
-      result = params[:result][0]
-      logger.info({from_line: result})
-      text_message = result['content']['text']
-      from_mid =result['content']['from']
   
-      client = LineClient.new(CHANNEL_ID, CHANNEL_SECRET, CHANNEL_MID, OUTBOUND_PROXY)
-      res = client.send([from_mid], text_message)
+      event = params["events"][0]
+      event_type = event["type"]
+      replyToken = event["replyToken"]
+  
+      case event_type
+      when "message"
+        input_text = event["message"]["text"]
+        output_text = input_text
+      end
+  
+      client = LineClient.new(CHANNEL_ACCESS_TOKEN, OUTBOUND_PROXY)
+      res = client.reply(replyToken, output_text)
   
       if res.status == 200
         logger.info({success: res})
       else
         logger.info({fail: res})
       end
+  
       render :nothing => true, status: :ok
     end
   
     private
-    # LINEからのアクセスか確認.
-    # 認証に成功すればtrueを返す。
-    # ref) https://developers.line.me/bot-api/getting-started-with-bot-api-trial#signature_validation
+    # verify access from LINE
     def is_validate_signature
-      signature = request.headers["X-LINE-ChannelSignature"]
+      signature = request.headers["X-LINE-Signature"]
       http_request_body = request.raw_post
       hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, CHANNEL_SECRET, http_request_body)
       signature_answer = Base64.strict_encode64(hash)
       signature == signature_answer
     end
-  end
+end
