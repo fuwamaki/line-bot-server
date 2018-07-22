3f57c6e8bb0659569d30b4da18d8e905b486e8b7
fuwamaki <yusaku_maki@fablic.co.jp>
2018-07-22 11:09:57 +0900

    route.rbにマッチしないルートが呼ばれた時の処理追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # resources :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/callback' => 'webhook#callback'
+  match '*unmatched_route', :to => 'application#raise_not_found!', :via => :all
 end
